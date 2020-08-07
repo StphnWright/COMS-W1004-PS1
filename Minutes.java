9ade601b33f7f31841cadaa2887f7d0cb5b3285e
Stephen Wright <62689098+StphnWright@users.noreply.github.com>
2020-08-07 13:37:35 -0700

    Update Minutes.java

--- Minutes.java
+++ Minutes.java
@@ -4,29 +4,30 @@
 import java.util.Scanner;
 
 public class Minutes {
-    
-    public static final void main(String[] args) {
-        
-        //Prompt to gather user input
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter your age in years, months, weeks, days, and hours.");
-        
-        //Numerical user input
-        int years = in.nextInt();
-        int months = in.nextInt();
-        int weeks = in.nextInt();
-        int days = in.nextInt();
-        int hours = in.nextInt();
-        System.out.println();
-        
-        //Output user input age chart
-        String ageChart = ("Your age chart: " + years + " years, " + months + " months, " + weeks + " weeks, " + days + " days, " + hours + " hours.");
-        System.out.println(ageChart);
-        System.out.println();
-        
-        //Output user input converted to minutes
-        int numYearMinutes = years*525600;
-        System.out.println("You have been alive for " + numYearMinutes + " minutes and counting...");
-        
-    }    
+
+	public static final void main(String[] args) {
+
+		//Prompt to gather user input
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter your age in years, months, weeks, days, and hours.");
+
+		//Numerical user input
+		int years = in.nextInt();
+		int months = in.nextInt();
+		int weeks = in.nextInt();
+		int days = in.nextInt();
+		int hours = in.nextInt();
+		System.out.println();
+
+		//Output user input age chart
+		String ageChart = ("Your age chart: " + years + " years, " + months + " months, "
+				+ weeks + " weeks, " + days + " days, " + hours + " hours.");
+		System.out.println(ageChart);
+		System.out.println();
+
+		//Output user input converted to minutes
+		int numYearMinutes = years*525600;
+		System.out.println("You have been alive for " + numYearMinutes + " minutes and counting...");
+
+	}    
 }
